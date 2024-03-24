67dbe98170e1d53851b2222cdbcb3a36e6e5bbae
bkmnk <164716580+bkmnk@users.noreply.github.com>
2024-03-24 13:41:32 -0300

    x

---
+++ build-deps.sh
@@ -0,0 +1,13 @@
+sudo apt-get update
+sudo apt update && sudo apt install --assume-yes curl
+sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
+sudo apt install -y nodejs
+sudo apt-get install npm
+cd /var/www/html
+sudo npm install --save puppeteer@~17.1.3
+sudo apt install libgconf-2-4 libatk1.0-0 libatk-bridge2.0-0 libgdk-pixbuf2.0-0 libgtk-3-0 libgbm-dev libnss3-dev libxss-dev
+apt-get install libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libnss3 libxss1 libasound2 libxtst6 xauth xvfb
+
+
+npm i
+
